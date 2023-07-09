7cb85babf27114772a30945c2e5e8fd5519b6df8
LifeViolation <128378329+LifeViolation@users.noreply.github.com>
2023-07-09 13:26:41 +0900

    rename widgets and separate widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,39 @@ import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'widgets/window_buttons.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const App());
   doWhenWindowReady(() {
     final win = appWindow;
-    const initialSize = Size(600, 450);
-    win.minSize = initialSize;
-    win.size = initialSize;
-    win.alignment = Alignment.center;
-    win.title = "Custom window with Flutter";
+    win.title = "Visual Studio Like GUI";
     win.show();
   });
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            body: Container(
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: MainWindow());
+  }
+}
+
+class MainWindow extends StatefulWidget {
+  const MainWindow({super.key});
+
+  @override
+  MainWindowState createState() => MainWindowState();
+}
+
+class MainWindowState extends State<MainWindow> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: WindowBorder(
+            width: 1,
+            color: const Color(0xFF7160e8),
+            child: Container(
                 color: const Color(0xFF1f1f1f),
                 child: Column(children: [
                   WindowTitleBarBox(
