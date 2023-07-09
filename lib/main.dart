4f4d12cb902b9fa4c48ab40f4e47c0f54448852f
LifeViolation <128378329+LifeViolation@users.noreply.github.com>
2023-07-09 14:31:42 +0900

    Update main.dart
    change border color on active|deactive

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:desktop_lifecycle/desktop_lifecycle.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 
@@ -30,12 +32,20 @@ class MainWindow extends StatefulWidget {
 }
 
 class MainWindowState extends State<MainWindow> {
+  final ValueListenable<bool> activeEvent = DesktopLifecycle.instance.isActive;
+  bool isActive = true;
+
   @override
   Widget build(BuildContext context) {
+    activeEvent.addListener(() {
+      setState(() {
+        isActive = activeEvent.value;
+      });
+    });
     return Scaffold(
         body: WindowBorder(
             width: 1,
-            color: const Color(0xFF7160e8),
+            color: isActive ? const Color(0xFF7160e8) : const Color(0xFF3D3D3D),
             child: Container(
                 color: const Color(0xFF1f1f1f),
                 child: Column(children: [
