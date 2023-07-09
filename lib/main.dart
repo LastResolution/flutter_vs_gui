970f81d543cddd7bfac16159e5309cd584f96281
LifeViolation <128378329+LifeViolation@users.noreply.github.com>
2023-07-09 17:08:47 +0900

    refactor: file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'package:desktop_lifecycle/desktop_lifecycle.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:flutter_vs_gui/widgets/main_window.dart';
 
 import 'util/url_launcher.dart';
-import 'widgets/window_buttons.dart';
 
 void main() {
   runApp(const App());
@@ -90,60 +88,5 @@ class App extends StatelessWidget {
                     ))
               ]),
             )));
-  }
-}
-
-class MainWindow extends StatefulWidget {
-  final String title;
-  final Widget child;
-  const MainWindow({super.key, required this.title, required this.child});
-
-  @override
-  MainWindowState createState() => MainWindowState();
-}
-
-class MainWindowState extends State<MainWindow> {
-  final ValueListenable<bool> activeEvent = DesktopLifecycle.instance.isActive;
-  bool isActive = true;
-
-  @override
-  Widget build(BuildContext context) {
-    activeEvent.addListener(() {
-      setState(() {
-        isActive = activeEvent.value;
-      });
-    });
-    return Scaffold(
-        body: WindowBorder(
-            width: 1,
-            color: isActive ? const Color(0xFF7160e8) : const Color(0xFF3D3D3D),
-            child: Container(
-                color: const Color(0xFF1f1f1f),
-                child: Column(children: [
-                  WindowTitleBarBox(
-                    child: Row(children: [
-                      Expanded(
-                          child: MoveWindow(
-                        child: Row(children: [
-                          Padding(
-                              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                              child: Image.asset(isActive
-                                  ? "images/active_icon.png"
-                                  : "images/deactive_icon.png")),
-                          Flexible(
-                              child: Text(
-                            widget.title,
-                            style: const TextStyle(color: Color(0xFFd6d6d6)),
-                            overflow: TextOverflow.fade,
-                            maxLines: 1,
-                            softWrap: false,
-                          ))
-                        ]),
-                      )),
-                      const WindowButtons()
-                    ]),
-                  ),
-                  Expanded(child: widget.child)
-                ]))));
   }
 }
---
+++ lib/widgets/main_window.dart
@@ -0,0 +1,60 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:desktop_lifecycle/desktop_lifecycle.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_vs_gui/widgets/window_buttons.dart';
+
+class MainWindow extends StatefulWidget {
+  final String title;
+  final Widget child;
+  const MainWindow({super.key, required this.title, required this.child});
+
+  @override
+  MainWindowState createState() => MainWindowState();
+}
+
+class MainWindowState extends State<MainWindow> {
+  final ValueListenable<bool> activeEvent = DesktopLifecycle.instance.isActive;
+  bool isActive = true;
+
+  @override
+  Widget build(BuildContext context) {
+    activeEvent.addListener(() {
+      setState(() {
+        isActive = activeEvent.value;
+      });
+    });
+    return Scaffold(
+        body: WindowBorder(
+            width: 1,
+            color: isActive ? const Color(0xFF7160e8) : const Color(0xFF3D3D3D),
+            child: Container(
+                color: const Color(0xFF1f1f1f),
+                child: Column(children: [
+                  WindowTitleBarBox(
+                    child: Row(children: [
+                      Expanded(
+                          child: MoveWindow(
+                        child: Row(children: [
+                          Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                              child: Image.asset(isActive
+                                  ? "images/active_icon.png"
+                                  : "images/deactive_icon.png")),
+                          Flexible(
+                              child: Text(
+                            widget.title,
+                            style: const TextStyle(color: Color(0xFFd6d6d6)),
+                            overflow: TextOverflow.fade,
+                            maxLines: 1,
+                            softWrap: false,
+                          ))
+                        ]),
+                      )),
+                      const WindowButtons()
+                    ]),
+                  ),
+                  Expanded(child: widget.child)
+                ]))));
+  }
+}
