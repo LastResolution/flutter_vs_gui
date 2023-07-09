d22550162ca1f322734892c48a75b255f423eb57
LifeViolation <128378329+LifeViolation@users.noreply.github.com>
2023-07-09 12:03:57 +0900

    add bitsdojo_window

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,8 +9,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Container()),
-    );
+        debugShowCheckedModeBanner: false, home: Scaffold(body: Container()));
   }
 }
