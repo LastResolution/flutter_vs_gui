eae5d0ee4536b6f488071d02f8984db308b5186a
LifeViolation <128378329+LifeViolation@users.noreply.github.com>
2023-07-09 12:42:10 +0900

    Update main.dart
    add control buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 
+import 'widgets/window_buttons.dart';
+
 void main() {
   runApp(MyApp());
+  doWhenWindowReady(() {
+    final win = appWindow;
+    const initialSize = Size(600, 450);
+    win.minSize = initialSize;
+    win.size = initialSize;
+    win.alignment = Alignment.center;
+    win.title = "Custom window with Flutter";
+    win.show();
+  });
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false, home: Scaffold(body: Container()));
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Container(
+                color: const Color(0xFF1f1f1f),
+                child: Column(children: [
+                  WindowTitleBarBox(
+                    child: Row(children: [
+                      Expanded(child: Container()),
+                      const WindowButtons()
+                    ]),
+                  )
+                ]))));
   }
 }
